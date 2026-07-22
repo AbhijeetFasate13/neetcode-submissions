861fdff93254592f28d1fa68359245c9a09f6f11
Abhijeet Fasate <96332156+AbhijeetFasate13@users.noreply.github.com>
2026-07-22 12:07:34 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n = temp.size();
+        stack<int>st;
+        vector<int>ans(n);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() and temp[st.top()]<=temp[i]){
+                st.pop();
+            }
+            if(!st.empty())
+            ans[i] = st.top()-i;
+            else
+            ans[i] = 0;
+            st.push(i);
+        }
+        return ans;
+    }
+};
